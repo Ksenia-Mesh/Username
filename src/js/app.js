1d32c5571c3fa0acbee7440d84741d06bb502438
Ksenia-Mesh <97454789+Ksenia-Mesh@users.noreply.github.com>
2022-12-16 09:13:19 +0300

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 export default class Validator {
   static validateUsername(username) {
-    const template1 = /^[^_\W\d-][\w-]*[^_\W\d-]+$/.test(username);
+    const template1 = /[A-Za-z][\w-]*[^_\W\d-]+$/.test(username);
     const template2 = /\d{4}/.test(username);
     return template1 && !template2;
   }
